f5ce9dee02c48a2c05af96c87794b2ab5b84d5d0
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2020-08-29 09:03:04 -0500

    Some more uopdates

--- users/models.py
+++ users/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
     # add additional fields in here
-    email = models.EmailField(('email address'), unique=True)
+    email = models.EmailField(('email address'))
     date_joined = models.DateTimeField(('date joined'), auto_now_add=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_seller = models.BooleanField(default=False)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.email
+        return self.username
+
+class Account(models.Model):
+    account = models.CharField(max_length=7)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
---
+++ utils/utils.py
@@ -0,0 +1,7 @@
+def get_email_from_netid(netid):
+    import subprocess
+    import shlex
+    bash_command = "curl -F \"netid={0}\" -F \"a=1\" -F \"form_type=advanced\" --url directory.northwestern.edu/?a=1".format(netid)
+    args = shlex.split(bash_command)
+    result = subprocess.run(args,stdout=subprocess.PIPE)
+    return result.stdout.decode("utf-8").split('class=\"email\" href=\"mailto:')[-1].split('">')[0]
